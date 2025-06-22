595afae0031b70d126657120b92d54424230ef70
Chaitanya-Shinde <shindechaitanya2002@gmail.com>
2025-06-23 00:20:07 +0530

    day 2 complete, refeshed basics about arrays, strings, and vectors

--- problems.cpp
+++ problems.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<string>
 using namespace std;
 
 vector<string> findMatchingFruits(){
@@ -27,8 +28,75 @@ int singleNumber(vector<int>& arr){
     return ans;
 }
 
+int reverseNumber(){
+    int num;
+    cout << "enter a positive number to reverse: ";
+    cin >> num;
+    int reversedNum=0;
+    while( num > 0){
+        int digit = num % 10;
+        reversedNum = reversedNum * 10 + digit;
+        num /= 10;
+
+    }
+    cout << endl<< reversedNum;
+    return 0;
+}
+
+string reverseString(){
+    string str;
+    cout << "Enter a string to reverse: " << endl;
+    cin >> str;
+    string reversedStr;
+    for(int i = str.length(); i >= 0; i--){
+        reversedStr = reversedStr + str[i];
+    }
+    cout<< reversedStr;
+    return reversedStr;
+}
+
+bool checkPalindromeNumber(int num){
+    // int num = 0;
+    // cout << "Enter a number: "<< endl;
+    // cin >> num;
+    int originalNum = num;
+    int reversedNum = 0;
+    while(num > 0){
+        int digit = num % 10;
+        reversedNum = reversedNum * 10 + digit;
+        num /= 10;
+    }
+    if(reversedNum == originalNum){
+        return true;
+    }else{
+        return false;
+    }
+    
+    return false;
+}
+
+bool checkPalindromeStr(string str){
+    string originalStr = str;
+    string reversedStr = "";
+    for(int i = str.length() - 1; i >= 0; i--){
+        reversedStr = reversedStr + str[i];
+    }
+    if(reversedStr == originalStr){
+        return true;
+    }else{
+        return false;
+    }
+    return false;
+}
+
 int main(){
     vector<int> nums = {1,1,2,3,3,4,5,5};
     //vector<string> result = findMatchingFruits();
-    cout << singleNumber(nums);
+    //cout << singleNumber(nums);
+    //reverseNumber();
+    //reverseString();
+    //cout << checkPalindromeNumber(121);
+    //cout << checkPalindromeStr("aabbaa");
+    
+
 }
